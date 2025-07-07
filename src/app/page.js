84a635185409fbefc25353c576a8a6921d4b16be
SnehaK-38 <sneha.k.3806@gmail.com>
2025-07-07 17:22:34 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -169,9 +169,8 @@ export default function HeroAbout() {
         }}
       >
         <div className="max-w-3xl mx-auto px-4">
-          <h2 className="text-3xl md:text-4xl font-bold mb-4">
-<h2> &quot;Healing doesn't mean the damage never existed. It means it no longer controls your life.&quot; </h2>
-          </h2>
+          <h2 className="text-3xl md:text-4xl font-bold mb-4">{`"Healing doesn't mean the damage never existed. It means it no longer controls your life."`}</h2>
+
           <p className="text-lg md:text-xl">— Akshay Dubey</p>
         </div>
       </section>
